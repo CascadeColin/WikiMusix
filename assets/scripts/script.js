06033ec9d51d71102f86e901f7765f72f027cd44
Colin Marshall <colin.marshall94@gmail.com>
2022-11-03 21:07:28 -0700

    added template to README, added notes to script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,7 +9,13 @@
 //////////////////////////////////////////////////////////
 
 /*
+include in head of HTML
+<script async src="https://cse.google.com/cse.js?cx=e40401ab6122d4604">
+</script>
 
+div to include GCS into page content
+customizable, see https://developers.google.com/custom-search/docs/element
+<div class="gcse-search"></div>
 
 
 
