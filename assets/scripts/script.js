3ab4a85f26e7cb140fa5f0c538df59309e2df743
mtownsend509 <mgtownsend509@gmail.com>
2022-11-07 20:37:41 -0800

    added an id on the input and button elements, and implemented the napster javascript to give us top songs, related artists, top albums and top album art on search. Did not make javascript update html with that information yet

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -21,3 +21,83 @@ customizable, see https://developers.google.com/custom-search/docs/element
 
 */
 
+searchBar = document.querySelector('#searchBar');
+button = document.querySelector('#button');
+
+button.addEventListener("click", getArtistID);
+
+//main function
+function getArtistID(e) {
+    e.preventDefault();
+    userInput = searchBar.value.toLowerCase();
+    userInput = userInput.replaceAll(' ','-')
+    var requestURL = 'https://api.napster.com/v2.2/artists/'+userInput+'?&apikey=Y2Q4NGE2MTMtMGI2Ni00ZGEwLWE3NWItNGFjMTMyYjg0NzYz'
+    fetch(requestURL) 
+        .then(function (response) {
+           return response.json();
+        })
+        .then(function(data) {
+            var artistId = data.artists[0].id;
+            similarArtists(artistId);
+            topSongs(artistId);
+            topAlbums(artistId);
+        })
+}
+
+//spits out top 3(#based off (i<3)) similar artists
+function similarArtists(artistId) {
+    var requestURL = 'https://api.napster.com/v2.2/artists/'+artistId+'/similar?limit=5&apikey=Y2Q4NGE2MTMtMGI2Ni00ZGEwLWE3NWItNGFjMTMyYjg0NzYz'
+    fetch(requestURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            for(i=0;i<3;i++) {
+                console.log(data.artists[i].name);
+                //domHook.children[i].innerHTML = data.artists[i].name;
+            }
+        })
+}
+
+//spits out top 3 albums & a src link for the top album art;
+function topAlbums(artistId) {
+    var requestURL = 'https://api.napster.com/v2.2/artists/'+artistId+'/albums/top?limit=5&apikey=Y2Q4NGE2MTMtMGI2Ni00ZGEwLWE3NWItNGFjMTMyYjg0NzYz'
+    fetch(requestURL) 
+        .then(function (response) {
+           return response.json();
+        })
+        .then(function(data) {
+            console.log(data)
+            console.log(data.albums[0].id)
+            albumId = data.albums[0].id;
+            for(i=0;i<3;i++) {
+             //   domHook.children[i].innerHTML = '#' + (i+1) + ':' + data.albums[i].name
+             console.log(data.albums[i].name)
+            }
+            var requestURL = 'https://api.napster.com/v2.2/albums/'+albumId+'/images?apikey=Y2Q4NGE2MTMtMGI2Ni00ZGEwLWE3NWItNGFjMTMyYjg0NzYz'
+            fetch(requestURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    //what's logges here is the img src
+                    console.log(data.images[0].url);
+                })
+        })
+}
+
+//spits out top 3 songs for artist
+function topSongs(artistId) {
+    var requestURL = 'https://api.napster.com/v2.2/artists/'+artistId+'/tracks/top?limit=5&apikey=Y2Q4NGE2MTMtMGI2Ni00ZGEwLWE3NWItNGFjMTMyYjg0NzYz'
+    fetch(requestURL) 
+        .then(function (response) {
+           return response.json();
+        })
+        .then(function(data) {
+            for(i=0;i<3;i++) {
+             //   domhook.children[i].innerHTML = data.tracks[i].name 
+             console.log(data.tracks[i].name);
+            }
+        })
+};
+
